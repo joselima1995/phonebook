f109e3b5a67e0108ae8697f01711ca7399038c47
fc51790 <fc51790@alunos.fc.ul.pt>
2025-06-12 12:21:39 +0100

    static files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 app.use(express.json())
+app.use(express.static('dist'))
 // app.use(morgan('tiny'))
 app.use(morgan(function (tokens, req, res) {
   return [
